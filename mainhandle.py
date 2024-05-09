7911d181ddaac42adda6c99d8d3eedd9b381c71f
22236bhs <22236@burnside.school.nz>
2024-05-10 11:56:52 +1200

    first commit

---
+++ mainhandle.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+DATABASE = "steam_db.db"
+
+EXECUTE = ('''
+SELECT name, hours, studios.studio_name
+FROM my_steam_library
+JOIN studios ON my_steam_library.studio_id = studios.id
+ORDER BY hours DESC;''')
+
+
+with sqlite3.connect(DATABASE) as f:
+    cursor = f.cursor()
+    cursor.execute(EXECUTE)
+    results = cursor.fetchall()
+
+print(f"{"name":<40}{"hours":<8}studio\n")
+for tup in results:
+    print(f"{tup[0]:<40}{tup[1]:<8}{tup[2]}")
