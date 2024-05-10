c616be24eedd14c55358e9ef26e493999608a72a
22236bhs <22236@burnside.school.nz>
2024-05-10 19:06:55 +1200

    Added function for calculating total hours

--- mainhandle.py
+++ mainhandle.py
@@ -7,18 +7,31 @@ DATABASE = "steam_db.db"
 
 EXECUTE = ('''
 SELECT name, hours, studios.studio_name
-FROM my_steam_library
-JOIN studios ON my_steam_library.studio_id = studios.id
+FROM steam_library
+JOIN studios ON steam_library.studio_id = studios.id
 ORDER BY hours DESC;''')
 
 def updatedatabasehours():
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
-        cursor.execute("SELECT id, game_id FROM my_steam_library;")
+        cursor.execute("SELECT id, game_id FROM steam_library;")
         results = cursor.fetchall()
         newresults = steam_hours_update.gethours(results)
         for tup in newresults:
-            cursor.execute(f"UPDATE my_steam_library SET hours = {tup[1]} WHERE id = {tup[0]}")
+            cursor.execute(f"UPDATE steam_library SET hours = {tup[1]} WHERE id = {tup[0]}")
+
+            
+def gettotalhours():
+    '''Returns the total hours of the database entries through STEAM (not through the db file)'''
+    with sqlite3.connect(DATABASE) as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT id, game_id FROM steam_library;")
+        results = cursor.fetchall()
+        results2 = steam_hours_update.gethours(results)
+        total = 0
+        for entry in results2:
+            total += entry[1]
+        return total
 
 
 with sqlite3.connect(DATABASE) as f:
